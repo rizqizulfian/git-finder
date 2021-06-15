00faa37220467c7e9f58b4e5859a64a946846de3
rizqizulfian <rizqizulfian@gmail.com>
2021-06-16 05:29:15 +0700

    create Git store state management redux

---
+++ src/store/const.js
@@ -0,0 +1,4 @@
+export const SET_USER = 'github/setUser'
+export const SET_REPOSITORIES = 'github/setRepositories'
+export const SET_LOADING = 'github/setLoading'
+export const SET_ERROR = 'github/setError'
